98b1bc53e9c5837fe985fc5edc512f3be1b0331f
xiayu <809700565@qq.com>
2020-09-15 17:25:34 +0800

    leetcode

---
+++ cmake-build-debug/CMakeFiles/leetcode.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/leetcode.dir/main.cpp.obj"
+  "leetcode.exe"
+  "leetcode.exe.manifest"
+  "leetcode.lib"
+  "leetcode.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/leetcode.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
